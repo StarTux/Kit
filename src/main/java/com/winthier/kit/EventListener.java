614b22660ec897d44475c063284a423bc32de6d9
StarTux <ths.loy@googlemail.com>
2022-07-17 23:59:50 +0200

    Use Core PlayerCache and Hud APIs

--- src/main/java/com/winthier/kit/EventListener.java
+++ src/main/java/com/winthier/kit/EventListener.java
@@ -1,8 +1,11 @@
 package com.winthier.kit;
 
-import com.cavetale.sidebar.PlayerSidebarEvent;
-import com.cavetale.sidebar.Priority;
+import com.cavetale.core.event.hud.PlayerHudEvent;
+import com.cavetale.core.event.hud.PlayerHudPriority;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,15 +17,17 @@ import static net.kyori.adventure.text.format.NamedTextColor.*;
 @RequiredArgsConstructor
 public final class EventListener implements Listener {
     private final KitPlugin plugin;
+    private static final Component NOTIFICATION = join(noSeparators(), text("You have a ", AQUA), text("/kit", YELLOW));
 
     protected void enable() {
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
-    public void onPlayerSidebar(PlayerSidebarEvent event) {
+    public void onPlayerHud(PlayerHudEvent event) {
         if (!plugin.sidebarList.contains(event.getPlayer().getUniqueId())) return;
         if (!event.getPlayer().hasPermission("kit.kit")) return;
-        event.add(plugin, Priority.HIGH, join(noSeparators(), text("You have a ", AQUA), text("/kit", YELLOW)));
+        event.sidebar(PlayerHudPriority.HIGH, List.of(NOTIFICATION));
+        event.bossbar(PlayerHudPriority.LOW, NOTIFICATION, BossBar.Color.YELLOW, BossBar.Overlay.PROGRESS, 1.0f);
     }
 }
